6959a899c907c1d449b3f9bcb0312ef8e63e5559
csy1777 <1946239233@qq.com>
2026-03-15 21:42:47 +0800

    增加了AdvancedCard和其Manager

--- Assets/Scripts/Managers/AdvancedCardManager.cs
+++ Assets/Scripts/Managers/AdvancedCardManager.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdvancedCardManager : MonoBehaviour
+public class AdvancedCardManager : SingleTon <AdvancedCardManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject woodenComponent;
+    public GameObject stoneComponent;
+    public GameObject tileComponent;
+    public GameObject decorativeComponent;
 }
