7c38733f0e0d8a8bf7d2c92edc8ed82c125551ec
csy1777 <1946239233@qq.com>
2026-03-16 11:21:15 +0800

    修改了卡片放置逻辑

--- Assets/Scripts/Managers/HandManager.cs
+++ Assets/Scripts/Managers/HandManager.cs
@@ -6,11 +6,47 @@ using UnityEngine;
 public class HandManager : SingleTon<HandManager>
 {
     public BaseCard currentBaseCard;
-
+    public float checkRadius=1;
+    public LayerMask checkLayer;
     private Vector3 handPos;
+    private Collider2D checkCollider;
+    
     private void Update()
     {
         FollowCursor();
+        
+        //如果手上有卡片,点击鼠标右键就能删除
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (currentBaseCard != null)
+            {
+                Destroy(currentBaseCard.gameObject);
+                ClearHand();
+            }
+        }
+
+        if (FindCell()&&Input.GetMouseButtonDown(0))
+        {
+            if (checkCollider != null)
+            {
+                Cell cell = checkCollider.GetComponent<Cell>();
+                {
+                    if (cell != null)
+                    {
+                        bool success=cell.AddBaseCard(currentBaseCard);
+                        if (success)
+                        {
+                            currentBaseCard.canClick=false;
+                            ClearHand();
+                        }
+                        else
+                        {
+                            Debug.Log("格子满了");
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private void FollowCursor()
@@ -29,8 +65,30 @@ public class HandManager : SingleTon<HandManager>
     {
         currentBaseCard=baseCard;
     }
-    public void ClearCard()
+    public void ClearHand()
     {
         currentBaseCard = null;
+    }
+    private bool FindCell()
+    {
+        if (currentBaseCard != null)
+        {
+            Collider2D cellCollider2D = Physics2D.OverlapCircle(currentBaseCard.transform.position, checkRadius, checkLayer );
+            if (cellCollider2D != null)
+            {
+                checkCollider = cellCollider2D;
+                return true;
+            }
+
+            return false;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (currentBaseCard != null)
+            Gizmos.DrawWireSphere(currentBaseCard.transform.position, checkRadius);
     }
 }
