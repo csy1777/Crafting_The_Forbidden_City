cf8500fe1de05a339645875ae833872013c89ae6
csy1777 <1946239233@qq.com>
2026-03-25 22:34:07 +0800

    Reapply "Reapply "Merge branch '邓分支' into 陈思宇分支""
    This reverts commit a5db84ad75a7b6ebf025dcf9bb6ca0e35ab29e48.

---
+++ Assets/Scripts/Menu/SettingPanelController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingPanelController : MonoBehaviour
+{
+    public GameObject settingsPanel;      // �������
+    public Button openButton;              // �����õİ�ť
+    public Button closeButton;             // �ر����õİ�ť
+
+    void Start()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+
+        if (openButton != null)
+            openButton.onClick.AddListener(OpenSettings);
+
+        if (closeButton != null)
+            closeButton.onClick.AddListener(CloseSettings);
+    }
+    public void OpenSettings()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(true);
+        }
+    }
+
+    public void CloseSettings()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+        }
+    }
+
+    public void ToggleSettings()
+    {
+        if (settingsPanel != null)
+        {
+            bool isActive = settingsPanel.activeSelf;
+            settingsPanel.SetActive(!isActive);
+        }
+    }
+}
