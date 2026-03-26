54ac4b933e6ef7a86d25cf59cb2e1718f0467143
csy1777 <1946239233@qq.com>
2026-03-26 16:45:56 +0800

    修改了卡牌素材,场景索引待添加

--- Assets/Scripts/Managers/HandManager.cs
+++ Assets/Scripts/Managers/HandManager.cs
@@ -258,28 +258,28 @@ public class HandManager : SingleTon<HandManager>
             AdvancedCard obj=Instantiate(woodenComponent);
             currentCard = obj;
             AudioManager.Instance.PlayClip(Config.AdvancedCard_Generate,1);
-           CraftingSystem.Instance.CraftItem("wood_component");
+           //CraftingSystem.Instance.CraftItem("wood_component");
         }
         else if (cardType == AdvancedCardType.stoneComponent)
         {
             AdvancedCard obj=Instantiate(stoneComponent);
             currentCard = obj;
             AudioManager.Instance.PlayClip(Config.AdvancedCard_Generate,1);
-            CraftingSystem.Instance.CraftItem("stone_component");
+            //CraftingSystem.Instance.CraftItem("stone_component");
         }
         else if (cardType == AdvancedCardType.tileComponent)
         {
             AdvancedCard obj=Instantiate(tileComponent);
             currentCard = obj;
             AudioManager.Instance.PlayClip(Config.AdvancedCard_Generate,1);
-           CraftingSystem.Instance.CraftItem("tile_component");
+           //CraftingSystem.Instance.CraftItem("tile_component");
         }
         else if (cardType == AdvancedCardType.decorativeComponent)
         {
             AdvancedCard obj=Instantiate(decorativeComponent);
             currentCard = obj;
             AudioManager.Instance.PlayClip(Config.AdvancedCard_Generate,1);
-            CraftingSystem.Instance.CraftItem("decoration_component");
+            //CraftingSystem.Instance.CraftItem("decoration_component");
         }
         else if (cardType == AdvancedCardType.none)
         {
